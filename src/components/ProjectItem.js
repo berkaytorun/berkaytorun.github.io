17baf59977f1c425375249a90ce0bdf6c13826d4
Ayzar <berkayk3211@gmail.com>
2022-08-10 16:55:12 +0200

    Changed behaviour of project page Mobile UI Fix

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
-function ProjectItem({ image, name, id, skills }) {
-	const navigate = useNavigate();
+function ProjectItem({ image, name, id, skills, url }) {
 	return (
 		<div
 			className='projectItem'
 			onClick={() => {
-				navigate('/project/' + id);
+				window.open(url, '_blank');
 			}}>
 			<div style={{ backgroundImage: `url(${image})` }} className='bgImage' />
 			<h1> {name} </h1>
